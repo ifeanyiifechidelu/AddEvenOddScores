824f1f170c6d535fb3368195ea1301f46b325aee
ifeanyiifechidelu <102835210+ifeanyiifechidelu@users.noreply.github.com>
2022-05-28 14:27:29 +0100

    Adding of file

---
+++ AddScores/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var num = new int[] { 3, 4, 8, 5, };
+Console.WriteLine(Addarray(num));
+
+static int Addarray(int[] num)
+{
+
+    int score = 0;
+    foreach (var item in num)
+    {
+        if (item == 8)
+        {
+            score += 5;
+        }
+        else if (item % 2 == 0)
+        {
+            score += 1;
+        }
+        else if (item % 2 != 1)
+        {
+            score += 3;
+        }
+
+
+    }
+    return score;
+}
